fe19ffae2847e1f8feb8c9c68ce550ede3d2e0a3
cyber_octopus <gsy@gsy123mailbox.com>
2023-02-17 10:54:28 +0800

    improved project0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -107,5 +107,14 @@ public class NBody {
             // pause 10 milliseconds
             StdDraw.pause(10);
         }
+
+        // output result for autograde
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", scale);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
     }
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,10 +1,10 @@
 public class Planet{
-    double xxPos;   
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double xxPos;   
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     /* 
      * planet constructor 1
