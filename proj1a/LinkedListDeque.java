979b2416d65de09c1285ce8920a4a4c3a57664f3
cyber_octopus <gsy@gsy123mailbox.com>
2023-03-02 13:33:17 +0800

    added removeLast() and get() to proj1a

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -84,25 +84,70 @@ public class LinkedListDeque<T>{
         return temp;
     }
 
+    /* Removes and returns the item at the back of the deque. If no such item exists, returns null. */
+    public T removeLast(){
+        if (size == 0){
+            return null;
+        }
+        T temp = sentinel.prev.first;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size--;
+        return  temp;
+    }
+
+    /* Gets the item at the given index, where 0 is the front. If no such item exists, returns null. */
+    public T get(int index){
+        if (index >= size){
+            return null;
+        }
+        Node p = sentinel.next;
+        while(index != 0){
+            p = p.next;
+            index--;
+        }
+        return p.first;
+    }
+
+
+    /*    private Node getHandler(int index, Node sentinel){
+            if (index >= size){
+
+            }
+        }*/
+
     public static void main(String[] args) {
         LinkedListDeque<String> L = new LinkedListDeque<>();
-        System.out.println(L.isEmpty());
+        System.out.println(L.get(0));
+        L.addLast("the 0 msg");
+        L.addLast("the 1 msg");
+        L.addLast("the 2 msg");
+        System.out.println(L.get(99));
+
+/*        System.out.println(L.isEmpty());
         L.addLast("the 0 msg");
         System.out.println("\n Remove first");
         System.out.println(L.removeFirst());
         L.addFirst("the 2nd msg");
         L.addFirst("the 3rd msg");
+        L.addFirst("the 10th msg");
+        L.addFirst("the 11th msg");
         L.removeFirst();
+        L.removeLast();
         System.out.println("\n Remove first");
-        System.out.println(L.removeFirst());
+        System.out.println(L.removeLast());
         System.out.println(L.removeFirst());
         L.addLast("the -1 msg ");
         L.addFirst("the 4rd msg");
+        L.removeLast();
+        System.out.println(L.removeLast());
+        System.out.println(L.removeLast());
+        L.addFirst("hahaha");
         System.out.println("\n print Size");
         System.out.println(L.size());
         System.out.println("\n isEmpty");
         System.out.println(L.isEmpty());
         System.out.println("\n print Deque");
-        L.printDeque();
+        L.printDeque();*/
     }
 }
